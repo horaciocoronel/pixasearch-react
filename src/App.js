e643bdd04cf11a61767d5c6dca2e1478458ad256
horaciocoronel <arielc@gmail.com>
2018-05-14 15:11:41 -0400

    Import components to main app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Search from './components/Search';
+import MainImage from './components/MainImage';
+import Thumbnails from './components/Thumbnails';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Search />
+      <div>
+        <Search />
+        <MainImage />
+        <Thumbnails />
+      </div>
     );
   }
 }
