d78b15d393ee072d1a70a9996173a6b0b7775766
horaciocoronel <arielc@gmail.com>
2018-05-15 01:23:25 -0400

    Add About component

---
+++ src/components/About.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export class About extends Component {
+  render() {
+    return (
+      <div className="about">
+        THIS IS IT
+        THIS IS IT
+        THIS IS IT
+        THIS IS IT
+        THIS IS IT
+        THIS IS IT
+        THIS IS IT
+        THIS IS IT
+        THIS IS IT
+        THIS IS IT
+        THIS IS IT
+        THIS IS IT
+        THIS IS IT
+        THIS IS IT
+        THIS IS IT
+        THIS IS IT
+        THIS IS IT
+      </div>
+    )
+  }
+}
+
+export default About
