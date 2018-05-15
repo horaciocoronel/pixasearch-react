37100fe4c8881f7b869849296207a73cae6c2a91
horaciocoronel <arielc@gmail.com>
2018-05-15 00:42:21 -0400

    Add constructor and API call

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
 import React, { Component } from 'react';
 import Search from './components/Search';
 import MainImage from './components/MainImage';
-import Thumbnails from './components/Thumbnails';
+import Images from './components/Images';
+import {images} from './LocalData';
 import './App.css';
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: images.hits,
+      currentImage: images.hits[14].largeImageURL
+    }
+    this.getImages = this.getImages.bind(this);
+    // this.setCurrentImage = this.setCurrentImage.bind(this);
+  }
+  componentDidMount() {
+    if(this.state.images.keys === null) {
+      console.log('Im here');
+      this.getImages();
+    }
+  }
+
+  setCurrentImage = (e) => {
+    this.setState({currentImage: images.hits[`${e}`].largeImageURL})
+  }
+  
+  
+  getImages = (query) => {
+    const url = `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_KEY}&q=${query}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          images: result.hits
+        });
+      },
+      (error) => {
+        alert("Couldn't reach the API server")
+      }
+    )
+  }
+
   render() {
     return (
-      <div>
+      <div className="container">
         <Search />
-        <MainImage />
-        <Thumbnails />
+        {console.log(this.state.currentImage)}
+        <MainImage imageUrl={this.state.currentImage} />
+        <Images images={this.state.images} setCurrentImage={this.setCurrentImage}/>
       </div>
     );
   }
