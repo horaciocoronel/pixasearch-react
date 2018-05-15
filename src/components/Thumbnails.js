7a38cb557713257de773d0cfbba360c34106586c
horaciocoronel <arielc@gmail.com>
2018-05-15 15:12:52 -0400

    Remove dead code

--- src/components/Thumbnails.js
+++ src/components/Thumbnails.js
@@ -3,17 +3,14 @@ import React, { Component } from 'react'
 export class Thumbnails extends Component {
 
   onImageClick = (e) => {
-    // console.log(this.props.location);
     this.props.setCurrentImage(this.props.location)
   }
   
 
   render() {
     return (
-      
         <div className="thumbnail"
         onClick={this.onImageClick}
-        // location={this.props.location}
         onMouseOver={this.onImageClick}
         style={{backgroundImage: `url(${this.props.thumbUrl})`}}>
         </div> 
