8b9ace6c67bcc2ffba85e4fe10615814e7311e2e
horaciocoronel <arielc@gmail.com>
2018-05-15 13:10:06 -0400

    Add content to about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Github from '../assets/github.svg';
+import Linkedin from '../assets/linkedin.svg';
 
 export class About extends Component {
   render() {
     return (
       <div className="about">
-        THIS IS IT
-        THIS IS IT
-        THIS IS IT
-        THIS IS IT
-        THIS IS IT
-        THIS IS IT
-        THIS IS IT
-        THIS IS IT
-        THIS IS IT
-        THIS IS IT
-        THIS IS IT
-        THIS IS IT
-        THIS IS IT
-        THIS IS IT
-        THIS IS IT
-        THIS IS IT
-        THIS IS IT
+        <div className="about-content">
+          <div className="about-top">
+            <p>
+              PixaSearch is an image search app built in React and powered by Pixabay API.
+            </p>
+          </div>
+          <div className="about-bottom">
+            <span style={{fontWeight: '300'}}>Built by</span> <span>Horacio Coronel</span>
+            <p>
+              <a href="https://github.com/horaciocoronel" target="_blank" title="Horacio Coronel Github">
+              <img src={Github} alt="Github Logo"/></a>
+              &nbsp;&nbsp;
+              <a href="https://www.linkedin.com/in/horacio-coronel/" target="_blank" title="Horacio Coronel Linkedin">
+              <img src={Linkedin} alt="Linkedin Logo"/></a>
+            </p>
+          </div>
+        </div>
       </div>
     )
   }
