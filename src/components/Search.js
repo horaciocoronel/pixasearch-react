cc38a858e2d8d214d2724db2f4533794daf0a07e
horaciocoronel <arielc@gmail.com>
2018-05-15 00:44:12 -0400

    Refactor search as flexbox

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import logo from '../assets/camera.svg';
 
 export class Search extends Component {
   render() {
     return (
       <div className="search">
+      <div className="left">
+        <img src={logo}
+        /><span> PixaSearch</span>
+      </div>
+      <div className="right">
         <input type="text" placeholder="Search image" />
+      </div>
       </div>
     )
   }
