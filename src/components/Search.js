6daff182b72a670a35c137dc11c1a66aaec64805
horaciocoronel <arielc@gmail.com>
2018-05-15 01:23:01 -0400

    Add icon menu

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import logo from '../assets/camera.svg';
+import menuIcon from '../assets/menu-icon.svg';
 
 export class Search extends Component {
   render() {
     return (
       <div className="search">
       <div className="left">
-        <img src={logo}
+        <img src={logo} alt="logo"
         /><span> PixaSearch</span>
       </div>
       <div className="right">
         <input type="text" placeholder="Search image" />
+        <img src={menuIcon} alt="menu-icon" onClick={this.props.toggleAbout} />
       </div>
       </div>
     )
