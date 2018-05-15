e2e9b28fac60b1b4945e6a13024fb002b714bbc3
horaciocoronel <arielc@gmail.com>
2018-05-15 01:23:18 -0400

    Add toggle menu function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Search from './components/Search';
 import MainImage from './components/MainImage';
+import About from './components/About';
 import Images from './components/Images';
+import AnimateHeight from 'react-animate-height';
 import {images} from './LocalData';
 import './App.css';
 class App extends Component {
@@ -9,17 +11,28 @@ class App extends Component {
     super(props);
     this.state = {
       images: images.hits,
-      currentImage: images.hits[14].largeImageURL
+      currentImage: images.hits[14].largeImageURL,
+      showAbout: false,
+      height: 0
     }
     this.getImages = this.getImages.bind(this);
     // this.setCurrentImage = this.setCurrentImage.bind(this);
   }
   componentDidMount() {
     if(this.state.images.keys === null) {
-      console.log('Im here');
       this.getImages();
     }
   }
+
+  toggleAbout = () => {
+    // this.setState({showAbout: !this.state.showAbout})
+    const { height } = this.state;
+    this.setState({
+      height: height === 0 ? 300 : 0,
+    });
+    console.log(this.state.height)
+  }
+  
 
   setCurrentImage = (e) => {
     this.setState({currentImage: images.hits[`${e}`].largeImageURL})
@@ -43,12 +56,20 @@ class App extends Component {
   }
 
   render() {
+    const { height } = this.state;
     return (
       <div className="container">
-        <Search />
+        <Search toggleAbout={this.toggleAbout} />
         {console.log(this.state.currentImage)}
         <MainImage imageUrl={this.state.currentImage} />
         <Images images={this.state.images} setCurrentImage={this.setCurrentImage}/>
+        <AnimateHeight
+          duration={ 500 }
+          className='about'
+          height={ height } // see props documentation bellow
+        >
+          <About/>
+        </AnimateHeight>
       </div>
     );
   }
